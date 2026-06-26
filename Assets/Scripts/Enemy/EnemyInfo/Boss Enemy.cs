cff45b4985a4d3726c4f3ff625e5f646d4ba8c9d
KimTaeYun <yuntaeyun123@gmail.com>
2026-06-26 11:09:31 +0900

    boss enemy prefab 추가, enemy 콜라이더 수정

--- Assets/Scripts/Enemy/EnemyInfo/Boss Enemy.cs
+++ Assets/Scripts/Enemy/EnemyInfo/Boss Enemy.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BossEnemy : MonoBehaviour
+public class BossEnemy : EnemyBase
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
